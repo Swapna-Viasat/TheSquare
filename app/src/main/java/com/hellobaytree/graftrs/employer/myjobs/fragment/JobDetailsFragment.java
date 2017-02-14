f20b5e2dd7c0dc5a082b304f4938809c62d3e78a
gherghelejiu <gherg001@gmail.com>
2017-02-14 15:38:11 +0200

    job details fix

--- app/src/main/java/com/hellobaytree/graftrs/employer/myjobs/fragment/JobDetailsFragment.java
+++ app/src/main/java/com/hellobaytree/graftrs/employer/myjobs/fragment/JobDetailsFragment.java
@@ -202,7 +202,7 @@ public class JobDetailsFragment extends Fragment {
             if (null != job.owner.picture) {
                 logo.setVisibility(View.VISIBLE);
                 Picasso.with(getContext())
-                        .load(job.company.logo)
+                        .load(job.owner.picture)
                         .into(logo);
             }
         }
