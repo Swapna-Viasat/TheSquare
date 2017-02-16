234c1eace5721fe17b5849ee76b458bace10a70e
Vadim Goroshevsky <vadim.goroshevsky@gmail.com>
2017-02-16 21:57:49 +0200

    SC-254

--- app/src/main/java/com/hellobaytree/graftrs/worker/myjobs/fragment/JobsFragment.java
+++ app/src/main/java/com/hellobaytree/graftrs/worker/myjobs/fragment/JobsFragment.java
@@ -7,7 +7,6 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.HorizontalScrollView;
 
 import com.hellobaytree.graftrs.R;
 import com.hellobaytree.graftrs.worker.myjobs.adapter.JobsPagerAdapter;
@@ -22,10 +21,10 @@ import butterknife.ButterKnife;
 public class JobsFragment extends Fragment {
 
     public static final String TAG = "JobsFragment";
-    private int lastPosition;
-    @BindView(R.id.worker_jobs_tablayout) TabLayout tabLayout;
-    @BindView(R.id.worker_jobs_pager) ViewPager viewPager;
-    @BindView(R.id.a) HorizontalScrollView tabScroll;
+    @BindView(R.id.worker_jobs_tablayout)
+    TabLayout tabLayout;
+    @BindView(R.id.worker_jobs_pager)
+    ViewPager viewPager;
 
     public static JobsFragment newInstance() {
         return new JobsFragment();
@@ -39,7 +38,8 @@ public class JobsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_worker_jobs, container, false);
-        ButterKnife.bind(this, view); return view;
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
@@ -47,29 +47,6 @@ public class JobsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         viewPager.setAdapter(new JobsPagerAdapter(getActivity(), getChildFragmentManager()));
         tabLayout.setupWithViewPager(viewPager);
-        viewPager.setOffscreenPageLimit(5);
-        viewPager.addOnPageChangeListener(listener);
+        viewPager.setOffscreenPageLimit(4);
     }
-
-    private ViewPager.OnPageChangeListener listener = new ViewPager.OnPageChangeListener() {
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-        }
-
-        @Override
-        public void onPageSelected(int position) {
-            if ((position - lastPosition) > 0) {
-                tabScroll.smoothScrollBy(tabLayout.getWidth() / 3, 0);
-            } else {
-                tabScroll.smoothScrollBy(-tabLayout.getWidth() / 3, 0);
-            }
-            lastPosition = position;
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
-
-        }
-    };
 }
