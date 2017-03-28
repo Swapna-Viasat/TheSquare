ae74c5703ad1c9d6c6277110ceba713c00421c8f
Vadim Goroshevsky <vadim.goroshevsky@gmail.com>
2017-03-28 14:04:46 +0300

    Prevent covering screen content with button when keyboard is raised (onboarding screens)

--- app/src/main/java/construction/thesquare/worker/onboarding/fragment/SelectTradeFragment.java
+++ app/src/main/java/construction/thesquare/worker/onboarding/fragment/SelectTradeFragment.java
@@ -13,6 +13,7 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -76,6 +77,8 @@ public class SelectTradeFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_select_trade, container, false);
+        getActivity().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         ButterKnife.bind(this, view);
         return view;
     }
