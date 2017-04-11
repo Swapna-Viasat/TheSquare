5e6d586308f8799f5639b82e5441b365ac9d2aa9
Vadim Goroshevsky <vadim.goroshevsky@gmail.com>
2017-04-11 15:31:34 +0300

    Add Connected banner in the worker side

--- app/src/main/java/construction/thesquare/worker/jobmatches/adapter/JobMatchesAdapter.java
+++ app/src/main/java/construction/thesquare/worker/jobmatches/adapter/JobMatchesAdapter.java
@@ -56,9 +56,11 @@ public class JobMatchesAdapter extends RecyclerView.Adapter<JobMatchesAdapter.Jo
     private int getBannerImage(Job job) {
         int result = 0;
         if (!CollectionUtils.isEmpty(job.application)) {
-            if (job.application.get(0).status.id == ApplicationStatus.STATUS_APPROVED)
-                result = R.drawable.workers_booked;
-            else if (job.application.get(0).status.id == ApplicationStatus.STATUS_PENDING) {
+            if (job.application.get(0).status.id == ApplicationStatus.STATUS_APPROVED) {
+                if (job.isConnect) result = R.drawable.worker_connected;
+                else
+                    result = R.drawable.workers_booked;
+            } else if (job.application.get(0).status.id == ApplicationStatus.STATUS_PENDING) {
                 if (job.application.get(0).isOffer)
                     result = R.drawable.workers_offered;
                 else result = R.drawable.workers_applied;
